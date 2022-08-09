e2684a5dfb6c7b15a535ab48cc4d881edf7a1fd5
Utkarsh Saxena <utkarshsaxena900@gmail.com>
2022-08-09 14:44:33 +0530

    update projects

---
+++ app/src/main/java/com/example/newsapp/architecture/NewsDatabase.kt
@@ -0,0 +1,41 @@
+package com.example.newsapp.architecture
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.newsapp.NewsModel
+
+@Database(entities = [NewsModel::class], version = 3, exportSchema = false)
+abstract class NewsDatabase : RoomDatabase()
+{
+
+    abstract fun newsDao(): NewsDao
+
+    companion object
+    {
+
+        @Volatile
+        private var INSTANCE: NewsDatabase? = null
+
+        fun getDataseClient(context: Context): NewsDatabase
+        {
+
+            if (INSTANCE != null) return INSTANCE!!
+
+            synchronized(this)
+            {
+
+                INSTANCE = Room
+                    .databaseBuilder(context, NewsDatabase::class.java, "LOGIN_DATABASE")
+                    .fallbackToDestructiveMigration()
+                    .build()
+
+                return INSTANCE!!
+
+            }
+        }
+
+    }
+
+}
