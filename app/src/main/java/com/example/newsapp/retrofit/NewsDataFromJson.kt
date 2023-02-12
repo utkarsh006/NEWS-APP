754c6e7cb32386658d1ee41eea8681315acb364a
Utkarsh Saxena <utkarshsaxena900@gmail.com>
2023-02-12 16:29:02 +0530

    files deleted

--- app/src/main/java/com/example/newsapp/retrofit/NewsDataFromJson.kt
+++
@@ -1,7 +0,0 @@
-package com.example.newsapp.retrofit
-
-data class NewsDataFromJson(
-    val articles: List<Article>,
-    val status: String,
-    val totalResults: Int
-)
--- app/src/main/java/com/example/newsapp/retrofit/Source.kt
+++
@@ -1,6 +0,0 @@
-package com.example.newsapp.retrofit
-
-data class Source(
-    val id: Any,
-    val name: String
-)
